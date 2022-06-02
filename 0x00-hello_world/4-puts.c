a210f755eec50b2dddb87784a1b194774552460f
Mulubi <mulubiwarren@gmail.com>
2022-06-02 10:59:54 -0700

    Qn4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*
- * Main function prints the phrase,
- * specified while the return function,
- * ends the program if it runs successfully
+ * main - function prints the phrase specified
+ * Return: 0 if exited without problems,
+ * otherwise non-zero
  */
 int main(void)
 {
