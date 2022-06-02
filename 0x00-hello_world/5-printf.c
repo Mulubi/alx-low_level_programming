192ce637cd109609a8a3754803da025e953effb0
Mulubi <mulubiwarren@gmail.com>
2022-06-02 11:05:24 -0700

    Qn5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@ include <stdio.h>
  */
 int main(void)
 {
-        puts("with proper grammar, but the outcome is a piece of art,");
+        printf("with proper grammar, but the outcome is a piece of art,");
         return (0);
 }
 
