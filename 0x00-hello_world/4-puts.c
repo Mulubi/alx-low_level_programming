a9d4c341fecf2c79057311e65bb79494badd420e
Mulubi <mulubiwarren@gmail.com>
2022-06-02 10:50:18 -0700

    Qn4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/* Main function prints the phrase specified while the return function ends the program if it runs successfully */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
