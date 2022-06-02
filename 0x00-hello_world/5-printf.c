2961078068b75e19d7775e825441bb69178af402
Mulubi <mulubiwarren@gmail.com>
2022-06-02 11:04:18 -0700

    Qn5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+include <stdio.h>
+/**
+ * main - function prints the phrase specified
+ * Return: 0 if exited without problems,
+ * otherwise non-zero
+ */
+int main(void)
+{
+        puts("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
+
