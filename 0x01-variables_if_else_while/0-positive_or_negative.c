58f8b7797660739eba33796af07517ca7bc0080a
Mulubi <mulubiwarren@gmail.com>
2022-06-03 07:44:11 -0700

    Qn0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - declares the type of int variable
+ * main - assigns a random number to int n everytime required,
+ * and Returns: 0 if the program executes successfuly.
  * if: collects the first input and checks whether true,
  * if not true the second function runs instead.
  * else: closes the block if both declared functions are not true.
@@ -15,12 +16,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n>0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	if (n==0)
+	if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	
 	return (0);
 }
