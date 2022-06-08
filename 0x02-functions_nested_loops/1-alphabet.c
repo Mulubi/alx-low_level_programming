4183225b607469dfcd59f9fe50dcc0847fcff7dc
Mulubi <mulubiwarren@gmail.com>
2022-06-08 06:55:18 -0700

    Qn1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/** 
+/**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
  *
- * Return: do not need to return anything if the return type is void. 
+ * Return: do not need to return anything if the return type is void.
  */
 
 void print_alphabet(void)
