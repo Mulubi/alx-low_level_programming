b54b8049929489106c3ff4252d0823b3b71b2358
Mulubi <mulubiwarren@gmail.com>
2022-06-09 08:52:09 -0700

    Qn4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - prints the numbers, from 0 to 9,
  * followed by a new line.
- *
+ * Do not print 2 and 4.
  * Return: void.
  */
 
@@ -13,7 +13,7 @@ void print_most_numbers(void)
 
 	for (; i <= '9'; i++)
 	{
-		if (i != '2' && i= '4')
+		if (i != '2' && i != '4')
 			_putchar(i);
 	}
 	_putchar('\n');
