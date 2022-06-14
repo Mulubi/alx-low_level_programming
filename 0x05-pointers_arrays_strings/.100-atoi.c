5a7dcc1eade6536f5fc42497fdcbc782ceee7c41
Mulubi <mulubiwarren@gmail.com>
2022-06-14 09:30:18 -0700

    Qn10

---
+++ 0x05-pointers_arrays_strings/.100-atoi.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _atoi - convert a string to an integer
+ * @s: string.
+ * Return: void.
+ */
+
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0)
+	}
+}
