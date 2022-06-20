da8ea3df3bef1ffc2864fc391356479d1ee6f128
Mulubi <mulubiwarren@gmail.com>
2022-06-20 09:05:50 -0700

    Qn1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area.
+ * @s: pointer to char
+ * @c: char parameter to found
+ * Return: to pointer s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (*s != 0)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
+}
