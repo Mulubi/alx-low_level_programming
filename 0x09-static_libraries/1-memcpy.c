a029df9d7346eeae1d9f046184df8a3112372aa6
Mulubi <mulubiwarren@gmail.com>
2022-06-24 10:39:45 -0700

    Qn0

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: destination to memory area
+ * @src: source memory area
+ * Return: the pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
