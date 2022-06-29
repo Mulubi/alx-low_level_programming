45aa58ff1f0c4633f4a57660651e1928d09c4abe
Mulubi <mulubiwarren@gmail.com>
2022-06-29 09:13:45 -0700

    Qn1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: counts
+ * @argv: arrays in args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
