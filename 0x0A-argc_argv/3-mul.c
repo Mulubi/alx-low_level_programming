fc4de60b93c19f2eb0dd46e483b92aefd09b69e7
Mulubi <mulubiwarren@gmail.com>
2022-06-29 09:44:45 -0700

    Qn3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,13 @@
 
 int main(int argc, char *argv[])
 {
+	mul = 0;
 	if (argc != 3)
-	mul = atoi(argv[1]) * atoi(argv[2]);
-
 	{
 		printf("Error\n");
 		return (1);
 	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", mul );
 	return (0);
 }
