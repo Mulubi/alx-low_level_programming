624afd883778474a8c2076a2bf3279b606e4c58c
Mulubi <mulubiwarren@gmail.com>
2022-07-04 17:39:09 -0700

    Qn0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,16 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
+ * dog_t - TYpedef for struct dog
  * struct dog - new type struct dog
  * @name: name type
  * @age: age type
  * @owner: owner type
  * Description: this is for a poppy
  */
+
+typedef struct dog dog_t;
 
 struct dog
 {
@@ -16,7 +19,6 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
