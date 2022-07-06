3477d4f418617abadbdd6ad70b3f86ab1b077048
Mulubi <mulubiwarren@gmail.com>
2022-07-06 08:03:37 -0700

    Contains the operator functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: a+b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - difference of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: a-b
+ */
+
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - product of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: a*b
+ */
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+
+/**
+ * op_div - result of the division of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: a/b
+ */
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+
+/**
+ * op_mod - remainder of the division of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: a%b
+ */
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
