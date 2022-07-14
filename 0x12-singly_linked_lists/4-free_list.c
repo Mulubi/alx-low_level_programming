196d570d5c103cb5b71ac88510a1c17dd56b3547
Mulubi <Mulubiwarren@gmail.com>
2022-07-14 09:39:45 -0700

    Qn4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: pointer to the start of the list
+ *
+ * Return: void.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *currlist, *nextlist;
+
+	currlist = head;
+	while (currlist !=NULL)
+	{
+		nextlist = currlist->nextlist;
+		free(currlist->str);
+		free(currlist);
+		currlist = nextlist;
+	}
+}
