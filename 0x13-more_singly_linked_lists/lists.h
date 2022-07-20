5b64873f66b881163dd97dabec78855a879b23a0
Mulubi <Mulubiwarren@gmail.com>
2022-07-20 07:51:14 -0700

    Prototype functions

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,31 @@
 #define LISTS_H_
 
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: pointer to the next node
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+}listint_t;
+
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: pointer to the next node
+ */
+
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+}listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
