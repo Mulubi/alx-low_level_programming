eb9af439970f6f1b9557e8212d242fc696293631
Mulubi <Mulubiwarren@gmail.com>
2022-07-22 01:14:16 -0700

    Qn0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,27 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i, b2;
 	unsigned int num;
+	int i, b2;
 
 	if (!b)
 		return (0);
+
 	num = 0;
 
 	for (i = 0; b[i] != '\0'; i++)
 		;
 	for (i--, b2 = 1; i >= 0; i--, b2 *= 2)
 	{
-		if (b[i] != && b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
+		}
+
 		if (b[i] & 1)
+		{
 			num += b2;
+		}
 	}
 
 	return (num);
