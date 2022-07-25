209d545da0d5290294df864369153d02f1019865
Mulubi <Mulubiwarren@gmail.com>
2022-07-25 08:01:14 -0700

    Qn3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,18 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * ch97 - checks for the correct number of arguments
+ * check97 - checks for the correct number of arguments
  * @argc: number of arguments present
  *
  * Return: nothing
  */
 
-void ch97(int argc)
+void check97(int argc)
 {
 	if (argc != 3)
 	{
@@ -22,7 +22,7 @@ void ch97(int argc)
 }
 
 /**
- * ch98 - checks if file_from exists and if readable
+ * check98 - checks if file_from exists and if readable
  * @check: checks if true or false
  * @file: file_from name
  * @fd_from: file descriptor of file_from, or -1
@@ -31,7 +31,7 @@ void ch97(int argc)
  * Return: nothing
  */
 
-void ch98(ssize_t check, char *file, int fd_from, int fd_to)
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
@@ -45,7 +45,7 @@ void ch98(ssize_t check, char *file, int fd_from, int fd_to)
 }
 
 /**
- * ch99 - checks that file_to was created and or can be written
+ * check99 - checks that file_to was created and or can be written
  * @check: checks if True or False
  * @file: file_to name
  * @fd_from: file descriptor of file_from, or -1
@@ -54,7 +54,7 @@ void ch98(ssize_t check, char *file, int fd_from, int fd_to)
  * Return: nothing
  */
 
-void ch99(ssize_t check, char *file, int fd_from, int fd_to)
+void check99(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
@@ -68,14 +68,14 @@ void ch99(ssize_t check, char *file, int fd_from, int fd_to)
 }
 
 /**
- * ch100 - checks that file descriptors were closed properly
+ * check100 - checks that file descriptors were closed properly
  * @check: checks if True or False
  * @fd: file descriptor
  *
  * Return: nothing
  */
 
-void ch100(int check, int fd)
+void check100(int check, int fd)
 {
 	if (check == -1)
 	{
@@ -99,30 +99,30 @@ int main(argc, char *argv[])
 	char buffer[1024];
 	mode_t file_perm;
 
-	ch97(argc);
+	check97(argc);
 	fd_from = open(argv[1], O_RDONLY);
 
-	ch98((ssize_t)fd_from, argv[1], -1, -1);
+	check98((ssize_t)fd_from, argv[1], -1, -1);
 	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
 
-	ch99((ssize_t)fd_to, argv[2], fd_from, -1);
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
 	lenr = 1024;
 
 	while (lenr == 1024)
 	{
 		lenr = read(fd_from, buffer, 1024);
-		ch98(lenr, argv[1], fd_from, fd_to);
+		check98(lenr, argv[1], fd_from, fd_to);
 
 		lenw = write(fd_to, buffer, lenr);
 
 		if (lenw != lenr)
 			lenw = -1;
-		ch99(lenw, argv[2], fd_from, fd_to);
+		check99(lenw, argv[2], fd_from, fd_to);
 	}
 	close_to = close(fd_to);
 	close_from = close(fd_from);
-	ch100(close_to, fd_to);
-	ch100(close_from, fd_from);
+	check100(close_to, fd_to);
+	check100(close_from, fd_from);
 	return (0);
 }
